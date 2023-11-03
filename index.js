f7e74be7a6a798e0f7cb876506e343d729f9cd00
Jacky <jackypengsmail@gmail.com>
2023-11-03 16:19:10 -0400

    Implement search to mealbutton event

--- index.js
+++ index.js
@@ -4,27 +4,30 @@
 const meal = document.getElementById("meal");
 const mealButton = document.querySelector("button");
 
-// mealButton.addEventListener("click", async () => {
-//     console.log(meal.value)
-//     const options = {
-//         method: "GET",
-//         url: "https://nutrition-by-api-ninjas.p.rapidapi.com/v1/nutrition",
-//         params: {
-//           query: meal.value,
-//         },
-//         headers: {
-//           "X-RapidAPI-Key": "6a627a9ed8msh2bb20f864871adcp1e222ajsnff6215037e1c",
-//           "X-RapidAPI-Host": "nutrition-by-api-ninjas.p.rapidapi.com",
-//         },
-//       };
+mealButton.addEventListener("click", async () => {
+  console.log(meal.value);
+  const options = {
+    method: "GET",
+    url: "https://nutrition-by-api-ninjas.p.rapidapi.com/v1/nutrition",
+    params: {
+      query: meal.value,
+    },
+    headers: {
+      "X-RapidAPI-Key": "6a627a9ed8msh2bb20f864871adcp1e222ajsnff6215037e1c",
+      "X-RapidAPI-Host": "nutrition-by-api-ninjas.p.rapidapi.com",
+    },
+  };
 
-//       try {
-//         const response = await axios.request(options);
-//         console.log(response.data);
-//       } catch (error) {
-//         console.error(error);
-//       }
-// })
+  try {
+    const response = await axios.request(options);
+    console.log(response.data);
+    const mealList = response.data;
+    totalMacrosDiv(mealList);
+    singleMacrosDiv(mealList);
+  } catch (error) {
+    console.error(error);
+  }
+});
 
 const mealList = [
   {
@@ -149,7 +152,7 @@ function totalMacrosDiv(mealList) {
   foodContainer.className = "total-macro-container";
   createFoodDivs(
     foodContainer,
-    meal.value,
+    "hotdog and ramen",
     round(totalCalories),
     round(totalCarbs),
     round(totalProtein),
